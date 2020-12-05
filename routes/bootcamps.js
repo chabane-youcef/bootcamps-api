18f7be362c03d6e322c771e06cad4587691a1cce
Chabane Youcef El Amine <joechabane98@gmail.com>
2020-12-05 17:33:16 +0100

    secured private routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -18,6 +18,8 @@ const courseRouter = require('./courses');
 
 const router = express.Router();
 
+const {protect} = require('../middleware/auth');
+
 //* Re-route into other resource router
 router.use('/:bootcampId/courses', courseRouter);
 
@@ -25,16 +27,16 @@ router
     .route('/radius/:zipcode/:distance')
     .get(getBootcampsInRadius);
 
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router.route('/:id/photo').put(protect, bootcampPhotoUpload);
 
 router
     .route('/')
     .get(advancedResults(Bootcamp, 'courses') ,getBootcamps)
-    .post(createBootcamp);
+    .post(protect, createBootcamp);
 
 router.route('/:id')
     .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+    .put(protect, updateBootcamp)
+    .delete(protect, deleteBootcamp);
 
 module.exports = router;
